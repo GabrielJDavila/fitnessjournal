2e053401b55d93be9bb949b077b993f5e2a88ae0
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-10-22 19:56:51 -0500

    Update to Index.css

--- fitnessjournal/src/pages/AllCategories.jsx
+++ fitnessjournal/src/pages/AllCategories.jsx
@@ -3,6 +3,13 @@ import { useState } from "react"
 export default function AllCategories() {
     const [toggleEditModal, setToggleEditModal] = useState(false)
     const [openConfirmDeleteModal, setOpenConfirmDeleteModal] = useState(false)
+    const [editCategoryTitle, setEditCategoryTitle] = useState({
+        title: ""
+    })
+    const [title, setTitle] = useState({
+        title: ""
+    })
+    console.log(editCategoryTitle.title)
 
     function toggleDelete() {
         setOpenConfirmDeleteModal(prev => !prev)
@@ -12,28 +19,48 @@ export default function AllCategories() {
         setToggleEditModal(prev => !prev)
     }
 
+    function handleChange(name, value) {
+        setEditCategoryTitle(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    function handleSubmit() {
+        
+    }
+
     const modalStyles = {
         position: "absolute",
         right: "50px",
         left: "50px",
-        background: "white",
-        boxShadow: "0 0 10px 1px black"
+        background: "white"
     }
 
     const editModal =
-        <div className="edit-modal" style={modalStyles}>
+        <form className="edit-modal" style={modalStyles}>
             <h2>Edit Category</h2>
-            <p>Cat. Name</p>
-            <p onClick={toggleEdit} className="cancel-btn">cancel</p>
-            <button className="confirm-btn">save</button>
-        </div>
+            <input
+                name="title"
+                onChange={e => handleChange(e.target.name, e.target.value)}
+                value={editCategoryTitle.title}
+                className="edit-cat-input"
+                placeholder="new category name"
+            />
+            <div className="edit-modal-btns-container">
+                <p onClick={toggleEdit} className="cancel-btn">cancel</p>
+                <button className="confirm-btn">save</button>
+            </div>
+        </form>
     
     const confirmDeleteModal =
         <div className="confirm-delete-modal" style={modalStyles}>
             <h2>Delete Category</h2>
             <p>Are you sure you want to delete Cat. Name?</p>
-            <p onClick={toggleDelete} className="cancel-btn">cancel</p>
-            <button className="confirm-btn">delete</button>
+            <div className="confirm-delete-modal-btns-container">
+                <p onClick={toggleDelete} className="cancel-btn">cancel</p>
+                <button className="confirm-btn">delete</button>
+            </div>
         </div>
 
     return (
@@ -48,7 +75,7 @@ export default function AllCategories() {
             </form>
             {toggleEditModal && editModal}
             {openConfirmDeleteModal && confirmDeleteModal}
-            <Category name={"Legs"} toggleEdit={toggleEdit} toggleDelete={toggleDelete}/>
+            <Category name={editCategoryTitle.title} toggleEdit={toggleEdit} toggleDelete={toggleDelete}/>
             <Category name={"Back"} toggleEdit={toggleEdit} toggleDelete={toggleDelete}/>
             <Category name={"Chest"} toggleEdit={toggleEdit} toggleDelete={toggleDelete}/>
         </div>
